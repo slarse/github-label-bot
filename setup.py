82d0c817d7109813522fd0e7a52541a9aab42898
Simon Larsén <slarse@kth.se>
2019-04-04 15:38:10 +0200

    Add Python package #7

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r", encoding="utf-8") as f:
+    readme = f.read()
+
+test_requirements = ["pytest>=4.0.0", "codecov", "pytest-cov"]
+required = []
+
+setup(
+    name="labelbot",
+    version="0.0.1",
+    description=(
+        "A GitHub label bot for allowing unprivileged users to label "
+        "with allowed labels."
+    ),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Joakim Croona, Simon Larsén",
+    author_email="jcroona@kth.se, slarse@kth.se",
+    license="MIT",
+    packages=find_packages(exclude=("tests", "docs")),
+    tests_require=test_requirements,
+    install_requires=required,
+    extras_require=dict(TEST=test_requirements),
+    include_package_data=True,
+    python_requires=">=3.7",
+)
