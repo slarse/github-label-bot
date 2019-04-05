fa159395f9408aa6c7a8fa73190eb88339a3877f
Simon Larsén <slarse@kth.se>
2019-04-05 11:55:53 +0200

    Add expected requirements

--- labelbot/bot.py
+++ labelbot/bot.py
@@ -1,7 +1,11 @@
+import json
+from jwcrypto import jwk
+import python_jwt
+
 # TODO remove this function.
 def placeholder():
     """This is just a placeholder function for initial project setup."""
-    return 42
+    return f"{jwk.__file__}\n{python_jwt.__file__}"
 
 
 def lambda_handler(event, context):
@@ -11,3 +15,7 @@ def lambda_handler(event, context):
     # env = os.environ['hej']
 
     return {"statusCode": 200, "body": json.dumps(placeholder())}
+
+
+if __name__ == "__main__":
+    print(placeholder())
