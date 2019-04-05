d5ad5b30c996ac9cb937fa08e948a045f7879eb8
Joakim Croona <jcroona@kth.se>
2019-04-05 11:02:00 +0200

    Add lambda function

--- labelbot/bot.py
+++ labelbot/bot.py
@@ -1,6 +1,13 @@
-
-
 # TODO remove this function.
 def placeholder():
     """This is just a placeholder function for initial project setup."""
     return 42
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    # number = event['X-GitHub-Event']
+    # data = json.loads(number)
+    # env = os.environ['hej']
+
+    return {"statusCode": 200, "body": json.dumps(placeholder())}
