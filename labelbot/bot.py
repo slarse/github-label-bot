39ea0a37e48f12399bf40c232e9ff4f63223ae41
Joakim Croona <jcroona@kth.se>
2019-04-11 21:20:07 +0200

    Retrieve PEM file, and extract relevant info from issue

--- labelbot/bot.py
+++ labelbot/bot.py
@@ -1,21 +1,42 @@
 import json
 from jwcrypto import jwk
 import python_jwt
+import os
+import boto3
+import botocore
+import labelbot.auth as auth
+import labelbot.github_api as github_api
 
-# TODO remove this function.
-def placeholder():
-    """This is just a placeholder function for initial project setup."""
-    return f"{jwk.__file__}\n{python_jwt.__file__}"
+
+
+
+def get_pem(bucket_name, key):
+    """Reads key from s3"""
+    s3 = boto3.resource('s3')
+    s3.Bucket(bucket_name).download_file(key, '/tmp/key.pem')
+    with open('/tmp/key.pem', "rb") as f:
+        pem = f.read()
+    return pem
 
 
 def lambda_handler(event, context):
     # TODO implement
-    # number = event['X-GitHub-Event']
-    # data = json.loads(number)
-    # env = os.environ['hej']
+    
+    body = json.loads(event["body"])
+    installation_id = body['installation']['id']
+    owner = body['repository']['owner']['login']
+    repo = body['repository']['name']
+    number = body['issue']['number']
+    issue_url = body['issue']['url']
+    issue_body = body['issue']['body']
 
-    return {"statusCode": 200, "body": json.dumps(placeholder())}
+    app_id = int(os.environ['APP_ID'])
+    bucket_name = os.environ['BUCKET_NAME']
+    bucket_key = os.environ['BUCKET_KEY']
+    pem = files.get_pem(bucket_name, bucket_key)
 
 
-if __name__ == "__main__":
-    print(placeholder())
+
+    return {"statusCode": 200, "body": json.dumps("temp")}
+
+
