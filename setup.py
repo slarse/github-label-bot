7a9b846fd1eaafaca9f2238cdcec9e881ccf1d12
Joakim Croona <jcroona@kth.se>
2019-04-17 11:51:31 +0200

    [fix] fix authentication and add tests for it

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", mode="r", encoding="utf-8") as f:
     readme = f.read()
 
-test_requirements = ["pytest>=4.0.0", "codecov", "pytest-cov", "responses"]
+test_requirements = ["pytest>=4.0.0", "codecov", "pytest-cov", "responses","pytest-mock"]
 required = ["python_jwt", "jwcrypto", "requests", "boto3"]
 
 setup(
